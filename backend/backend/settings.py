9d4f1925c7c485aaace9e5969ddf266a80971d51
palladium <bereg9812@yandex.ru>
2023-09-17 12:27:14 +0000

    saving taski

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,13 +1,16 @@
+import sentry_sdk
+
+from sentry_sdk.integrations.django import DjangoIntegration
+
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
+DEBUG = False
 
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['158.160.8.42', '127.0.0.1', 'localhost', 'palladium']
 
 
 # Application definition
@@ -55,7 +58,14 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'backend.wsgi.application'
 
-
+sentry_sdk.init(
+    dsn="https://65b89a7da892a38627398f1fff6e6a0b@o4505874017615872.ingest.sentry.io/4505874021548032",
+    integrations=[
+        DjangoIntegration(),
+    ],
+    traces_sample_rate=1.0,
+    send_default_pii=True
+)
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -103,7 +113,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static_backend'
+
+STATIC_ROOT = BASE_DIR/'static_backend'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field

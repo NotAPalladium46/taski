f774a5548ea3cd7353dab779bafaee50fe7d56a1
palladium <bereg9812@yandex.ru>
2023-09-17 22:48:10 +0000

    vital changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 DEBUG = False
 
-ALLOWED_HOSTS = ['158.160.8.42', '127.0.0.1', 'localhost', 'palladium']
+ALLOWED_HOSTS = ['158.160.8.42', '127.0.0.1', 'localhost', 'kittygrammm.zapto.org', 'taski.zapto.org']
 
 
 # Application definition
